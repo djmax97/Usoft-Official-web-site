a6febfebb962b988679ca56afa7e496c92cb0216
DayanJ <1997dayan@gmail.com>
2024-07-20 10:04:18 +0530

    image error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: HeaderComponent,
+        component: MainComponent,
         data: {
           title: "Dayan"
         }
